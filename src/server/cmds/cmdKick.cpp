f456746f0d89d7d06574198b5ad75a11fbfb9085
Romil <Shisoo64@gmail.com>
2023-09-25 15:30:01 +0200

    Cmd kick Update

--- src/server/cmds/cmdKick.cpp
+++ src/server/cmds/cmdKick.cpp
@@ -6,7 +6,7 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 16:19:09 by bchabot           #+#    #+#             */
-/*   Updated: 2023/09/22 13:30:00 by rlaforge         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:28:22 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void Server::cmdKick(Client &client, std::stringstream &msg) {
 	std::string			user;
 	std::string			arg;
 
-	// msg.ignore(512, ' ');
 	std::getline(msg, channel, ' ');
 	std::getline(msg, user, ' ');
 	std::getline(msg, arg);
 
+	if (!arg.empty()) {
+		sendMessage(client.getUserFd(), ERR_TOOMUCHPARAMS(client.getNickname(), channel));
+		return ;
+	}
 	if (!channel.empty() && channel[0] == '#') {
 		std::map<std::string, Channel>::iterator it = channels.find(channel);
 		if (it != channels.end() && it->second.isUserMember(user)) {
