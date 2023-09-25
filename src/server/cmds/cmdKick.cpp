6f5789ee17737a2e5ccfaa27a3763f07bc43c60c
barondugroove <chabot.benjamin@gmail.com>
2023-09-25 16:49:57 +0200

    KICK un peu mieu, repush pour netcat

--- src/server/cmds/cmdKick.cpp
+++ src/server/cmds/cmdKick.cpp
@@ -6,11 +6,7 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 16:19:09 by bchabot           #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2023/09/25 15:28:22 by rlaforge         ###   ########.fr       */
-=======
-/*   Updated: 2023/09/25 16:12:34 by bchabot          ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2023/09/25 16:36:48 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +21,9 @@ void Server::cmdKick(Client &client, std::stringstream &msg) {
 	std::getline(msg, user, ' ');
 	std::getline(msg, arg);
 
-<<<<<<< Updated upstream
-	if (!arg.empty()) {
-		sendMessage(client.getUserFd(), ERR_TOOMUCHPARAMS(client.getNickname(), channel));
-		return ;
-	}
-	if (!channel.empty() && channel[0] == '#') {
-		std::map<std::string, Channel>::iterator it = channels.find(channel);
-		if (it != channels.end() && it->second.isUserMember(user)) {
-			
-			if (!it->second.isUserOperator(client.getNickname())) {
-				sendMessage(client.getUserFd(), ERR_CHANOPRIVSNEEDED(client.getNickname(), channel));
-				return ;
-			}
-			it->second.eraseUser(user);
-		}
-		else
-			sendMessage(client.getUserFd(), ERR_NOTONCHANNEL(client.getNickname(), channel));
-=======
 	if (channel.empty() || channel[0] != '#' || user.empty() || !arg.empty()) {
 		sendMessage(client.getUserFd(), ERR_NEEDMOREPARAMS(client.getNickname(), channel));
 		return ;
->>>>>>> Stashed changes
 	}
 
 	std::map<std::string, Channel>::iterator it = channels.find(channel);
@@ -67,5 +44,4 @@ void Server::cmdKick(Client &client, std::stringstream &msg) {
 		return ;
 	}
 	it->second.eraseUser(user);
-	sendMessage()
 }
