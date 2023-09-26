97b1d7cdcf0f8ed0e73a67e3f9d160901aa69b34
Romil <Shisoo64@gmail.com>
2023-09-26 03:09:33 +0200

    Error codes OK?

--- includes/define.hpp
+++ includes/define.hpp
@@ -6,7 +6,7 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:10:03 by bchabot           #+#    #+#             */
-/*   Updated: 2023/09/26 02:46:42 by rlaforge         ###   ########.fr       */
+/*   Updated: 2023/09/26 03:08:46 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@
 #define MAXCLIENTS 1000
 
 //ERROR MESSAGES
-#define ERR_NOTREGISTERED(client) (client + " :You have not registered\r\n")
-#define ERR_CMDNOTFOUND(client) (client + " :Command not found\r\n")
-#define ERR_NOSUCHCHANNEL(client, channel) (client + " " + channel + " :No such channel\r\n")
-#define ERR_NOSUCHNICK(client, nickname) (client + " " + nickname + " :No such nickname\r\n")
-#define ERR_CHANOPRIVSNEEDED(client, channel) (client + " " + channel + " :You're not channel operator\r\n")
-#define ERR_NOTONCHANNEL(client, channel) (client + " " + channel + " :You're not on that channel\r\n")
-#define ERR_NEEDMOREPARAMS(client, cmd) (client + " " + cmd + " :Not enough parameters\r\n")
+#define ERR_NOTREGISTERED(client) (": 451 " + client + " :You have not registered\r\n")
+#define ERR_CMDNOTFOUND(client) (": 421 " + client + " :Command not found\r\n")
+#define ERR_NOSUCHCHANNEL(client, channel) (": 403 " + client + " " + channel + " :No such channel\r\n")
+#define ERR_NOSUCHNICK(client, nickname) (": 401 " + client + " " + nickname + " :No such nickname\r\n")
+#define ERR_CHANOPRIVSNEEDED(client, channel) (": 482 " + client + " " + channel + " :You're not channel operator\r\n")
+#define ERR_NOTONCHANNEL(client, channel) (": 442 " + client + " " + channel + " :You're not on that channel\r\n")
+#define ERR_NEEDMOREPARAMS(client, cmd) (": 461 " + client + " " + cmd + " :Not enough parameters\r\n")
 #define ERR_TOOMUCHPARAMS(client, cmd) (client + " " + cmd + " :Too much parameters\r\n")
-#define ERR_ALREADYREGISTERED(client) (client + " :You may not reregister\r\n")
+#define ERR_ALREADYREGISTERED(client) (": 462 " + client + " :You may not reregister\r\n")
 #define ERR_NICKNAMEINUSE(client, nick) (": 433 * " + client + " " + nick + " :Nickname is already in use\r\n")
-#define ERR_CHANNELISFULL(client, channel) (client + " " + channel + " :Cannot join channel (+l)\r\n")
-#define ERR_INVITEONLYCHAN(client, channel) (client + " " + channel + " :Cannot join channel (+i)\r\n")
-#define ERR_BADCHANNELKEY(client, channel) (client + " " + channel + " :Cannot join channel (+k)\r\n")
-#define ERR_USERNOTINCHANNEL(client, nick, channel) (client + " " + nick + " " + channel + " :They aren't on that channel\r\n")
-#define ERR_CANNOTSENDTOCHAN(client, channel) (client + " " + channel + " :Cannot send to channel\r\n")
-#define ERR_USERONCHANNEL(client, nick, channel) (client + " " + nick + " " + channel + " :is already on channel\r\n")
-#define ERR_PASSWDMISMATCH(client) (client + " :Password incorrect\r\n")
-#define ERR_UMODEUNKNOWNFLAG(client) (client + " :Unknown MODE flag\r\n")
-#define ERR_ERRONEUSNAME(client, nickname, name) (client + " " + nickname + " :Erroneus " + name + "\r\n")
-#define ERR_NOTEXTTOSEND(client) (client + " :No text to send\r\n")
-#define ERR_NORECIPIENT(client) (client + " :No recipient given PRIVMSG\r\n")
+#define ERR_CHANNELISFULL(client, channel) (": 471 " + client + " " + channel + " :Cannot join channel (+l)\r\n")
+#define ERR_INVITEONLYCHAN(client, channel) (": 473 " + client + " " + channel + " :Cannot join channel (+i)\r\n")
+#define ERR_BADCHANNELKEY(client, channel) (": 475 " + client + " " + channel + " :Cannot join channel (+k)\r\n")
+#define ERR_USERNOTINCHANNEL(client, nick, channel) (": 441 " + client + " " + nick + " " + channel + " :They aren't on that channel\r\n")
+#define ERR_CANNOTSENDTOCHAN(client, channel) (": 404 " + client + " " + channel + " :Cannot send to channel\r\n")
+#define ERR_USERONCHANNEL(client, nick, channel) (": 303 " + client + " " + nick + " " + channel + " :is already on channel\r\n")
+#define ERR_PASSWDMISMATCH(client) (": 464 " + client + " :Password incorrect\r\n")
+#define ERR_UMODEUNKNOWNFLAG(client) (": 501 " + client + " :Unknown MODE flag\r\n")
+#define ERR_ERRONEUSNAME(client, nickname, name) (": 432 " + client + " " + nickname + " :Erroneus " + name + "\r\n")
+#define ERR_NOTEXTTOSEND(client) (": 412 " + client + " :No text to send\r\n")
+#define ERR_NORECIPIENT(client) (": 411 " + client + " :No recipient given PRIVMSG\r\n")
 
 //PRIVMSG BUILDERS
 #define CHANNEL_MESSAGES(client, channel, msg) (":" + client + " PRIVMSG " + channel + " :" + msg + "\r\n")
